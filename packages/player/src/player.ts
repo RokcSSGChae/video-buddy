89fe573a41594d6a7dc21193e5415ea088006589
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2024-10-28 11:48:39 +0900

    [player] :sparkles: add getThumbnail, getCurrentTime method #3 (#9)
    Co-authored-by: 박석채 <schaepark@smilegate.com>

--- packages/player/src/player.ts
+++ packages/player/src/player.ts
@@ -20,19 +20,49 @@ export default class Player {
         container.appendChild(this.video);
     }
 
-    src(source: string): void {
-        this.video.src = source;
-    }
-  
     play(): void {
         this.video.play();
     }
-  
+
     pause(): void {
         this.video.pause();
     }
   
     getVideoElement(): HTMLVideoElement {
         return this.video;
+    }
+
+    getCurrentTime(): number {
+        return this.video.currentTime;
+    }
+
+    async getThumbnail(time = this.getCurrentTime()): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const isAtCurrentTime = time === this.getCurrentTime();
+
+            const captureThumbnail = () => {
+                const canvas = document.createElement('canvas');
+                canvas.width = this.video.videoWidth;
+                canvas.height = this.video.videoHeight;
+    
+                const context = canvas.getContext('2d');
+                if (context) {
+                    context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
+                    resolve(canvas.toDataURL('image/png'));
+                } else {
+                    reject(new Error('Failed to get 2D context.'));
+                }
+            };
+    
+            if (isAtCurrentTime) captureThumbnail();
+            else {
+                const onSeeked = () => {
+                    this.video.removeEventListener('seeked', onSeeked);
+                    captureThumbnail();
+                };
+                this.video.addEventListener('seeked', onSeeked, { once: true });
+                this.video.currentTime = time;
+            }
+        });
     }
 }
