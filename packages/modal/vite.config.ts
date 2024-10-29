0f4694951241433683da0c70ccbadc4a46886877
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2024-10-29 20:31:31 +0900

    [modal] :heavy_plus_sign: vite babel plugin 연동 #26 (#27)
    * add babel plugin
    
    * update package.json
    
    * update vite config
    
    ---------
    
    Co-authored-by: 박석채B <schaepark@smilegate.com>

--- packages/modal/vite.config.ts
+++ packages/modal/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import { babel } from '@rollup/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -31,7 +32,34 @@ export default defineConfig({
                     const fileName = names?.at(-1);
                     return fileName?.endsWith('.css') ? 'css/[name][extname]' : '';
                 }
-            }
+            },
+            plugins: [
+                babel({
+                    babelHelpers: 'bundled',
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                targets: {
+                                    browsers: [
+                                        'last 3 major versions',
+                                        'Firefox ESR',
+                                        'Chrome >= 53',
+                                        'not dead',
+                                        'not ie 11',
+                                        'not baidu 7',
+                                        'not and_qq 11',
+                                        'not and_uc 12',
+                                        'not op_mini all'
+                                    ]
+                                }
+                            }
+                        ]
+                    ],
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                    exclude: 'node_modules/**'
+                })
+            ]
         }
     }
 });
